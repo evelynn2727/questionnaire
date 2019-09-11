c3bcab8d65ff53ce008c815bcaddc4cb396ce897
evelynn2727 <totalnieniewybaczalne.ep@gmail.com>
2019-09-11 20:51:23 +0200

    refactoring

--- polls/models.py
+++ polls/models.py
@@ -5,18 +5,28 @@ from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
+    #choice fields
+    a_text= models.CharField(max_length=200, default=0)
+    b_text= models.CharField(max_length=200, default=0)
+    c_text= models.CharField(max_length=200, default=0)
+    CHOICE=[(a_text, b_text, c_text)]
+    #################
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
+    ##############################
+    def __str__(self):
+        return self.a_text
+    def __str__(self):
+        return self.b_text
+    def __str__(self):
+        return self.c_text
+    #########################
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
-CHOICES=[('select1','select 1'),
-         ('select2','select 2')]
-
 class Choice(models.Model):
-    like = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text=models.CharField(max_length=200)
     votes= models.IntegerField(default=0)
